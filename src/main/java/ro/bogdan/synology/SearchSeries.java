0951664ceecc1bd15e50b284bae60abf145075fd
Bogdan Bala <bogdan.bala@avira.com>
2015-05-18 11:31:12 +0300

    s

--- src/main/java/ro/bogdan/synology/SearchSeries.java
+++ src/main/java/ro/bogdan/synology/SearchSeries.java
@@ -13,21 +13,33 @@ import org.apache.log4j.Logger;
 @WebServlet("/searchSeries")
 public class SearchSeries extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	
+
 	private final Logger logger = Logger.getLogger(SearchSeries.class);
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		String query = req.getParameter("query").replaceAll("\\s", "\\.");
+		String query = req.getParameter("query");
+		if (query != null) {
+			query = query.replaceAll("\\s", "\\.");
+		}
 		String serie = req.getParameter("serie");
+		if(serie!=null){
+			serie = serie.trim();
+		}
 		String firstEpisode = req.getParameter("firstEpisode");
 		String lastEpisode = req.getParameter("lastEpisode");
+		String ignoreMissing = req.getParameter("ignoreMissing");
 		String onlyFree = req.getParameter("onlyFree");
 		String onlyAgregated = req.getParameter("onlyAgregated");
 		String latestEpisode = req.getParameter("latestEpisode");
+		String latestEpisodes = req.getParameter("latestEpisodes");
+
+		String lg = "Params: query:" + query + " serie:" + serie + " firstEpisode:" + firstEpisode + " lastEpisode:"
+		        + lastEpisode + " ignore missing:" + ignoreMissing + " onlyFree:" + onlyFree + " onlyAgregated:" + onlyAgregated
+		        + " latestEpisode:" + latestEpisode + " latestEpisodes:" + latestEpisodes;
+		// logger.info();
 		
-		logger.info("Params: query:"+query+" serie:"+serie+" firstEpisode:"+firstEpisode+" lastEpisode:"+lastEpisode
-				+" onlyFree:"+onlyFree+" onlyAgregated:"+onlyAgregated+" latestEpisode:"+latestEpisode);
+		resp.getOutputStream().print(lg);
 	}
 
 	@Override
